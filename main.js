ec147372d6abadb3c550ed1356726802dba19a66
James Gennoy <james@gennoy.com>
2015-11-25 11:59:29 -0800

    Added preference to select the number of seconds before the search box is cleared.

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-var viewFor = require('sdk/view/core').viewFor;
 var browserWindows = require('sdk/windows').browserWindows;
+var clearTimeout = require('sdk/timers').clearTimeout;
+var setTimeout = require('sdk/timers').setTimeout;
+var simplePrefs = require('sdk/simple-prefs');
+var viewFor = require('sdk/view/core').viewFor;
 
 var SearchScrub = {
 
     originalSearchCommand: undefined,
 
+    timeoutId: undefined,
+
     init: function(browserWindow) {
+        var _seconds = simplePrefs.prefs.seconds;
+        if (isNaN(_seconds)) {
+            _seconds = 0;
+        }
         if (browserWindow) {
             var _chromeWindow = viewFor(browserWindow);
             if (_chromeWindow) {
@@ -15,7 +24,11 @@ var SearchScrub = {
                     _searchbar.value = '';
                     _searchbar.handleSearchCommand = function() {
                         var _results = SearchScrub.originalSearchCommand.apply(this, arguments);
-                        this.value = '';
+                        if (SearchScrub.timeoutId) {
+                            clearTimeout(SearchScrub.timeoutId);
+                            SearchScrub.timeoutId = undefined;
+                        }
+                        SearchScrub.timeoutId = setTimeout(function() {_searchbar.value = '';}, _seconds * 1000);
                         return _results;
                     };
                 }
@@ -35,6 +48,14 @@ var SearchScrub = {
         }
     }
 };
+
+simplePrefs.on('', function() {
+    // Remove and then re-init on exsting windows
+    for(var x = 0; x < browserWindows.length; x++) {
+        SearchScrub.remove(browserWindows[x]);
+        SearchScrub.init(browserWindows[x]);
+    }
+});
 
 exports.main = function() {
     // Init on any new window
